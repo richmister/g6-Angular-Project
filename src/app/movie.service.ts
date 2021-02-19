cbfcfc49e5033c2c87083fb029c0b709a64e623c
jotabraham <jotabraham@gmail.com>
2021-02-19 15:56:34 -0500

    http and forms modules and api service

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -5,5 +5,10 @@ import { Injectable } from '@angular/core';
 })
 export class MovieService {
 
+  api_key:string = "8fb9e4a1ca7fb6bda4f2fc9edb0c5e09";
+  tmdbTrendingUrl:string = "https://api.themoviedb.org/3/trending/movie/week";
+  tmdbSearchUrl:string = "https://api.themoviedb.org/3/search/movie";
+  
+
   constructor() { }
 }
