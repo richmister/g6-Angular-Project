94986ad9dbfd40666aac177735f3e2d778f6d6de
richmister <75438128+richmister@users.noreply.github.com>
2021-02-22 10:34:11 -0500

    angp1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: NotFoundComponent }, // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/search-movies-form/search-movies-form.component.ts
+++ src/app/search-movies-form/search-movies-form.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-search-movies-form',
   templateUrl: './search-movies-form.component.html',
-  styleUrls: ['./search-movies-form.component.css']
+  styleUrls: ['./search-movies-form.component.css'],
 })
 export class SearchMoviesFormComponent implements OnInit {
+  @Output() searchEvent = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  emitSearch = (form: NgForm) => {
+    console.log(form);
+    this.searchEvent.emit(form.form.value.searchTerm);
+    this.searchEvent.emit(form.form.value.genre);
+    this.searchEvent.emit(form.form.value.rating);
+  };
 }
