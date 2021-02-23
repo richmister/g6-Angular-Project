15dc397cdeead328343ce47c9ab44ec46912b1d5
richmister <75438128+richmister@users.noreply.github.com>
2021-02-23 17:47:22 -0500

    angp3

--- src/app/search-movies-form/search-movies-form.component.ts
+++ src/app/search-movies-form/search-movies-form.component.ts
@@ -2,38 +2,39 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MovieService } from '../movie.service';
 
-
 @Component({
   selector: 'app-search-movies-form',
   templateUrl: './search-movies-form.component.html',
   styleUrls: ['./search-movies-form.component.css'],
 })
 export class SearchMoviesFormComponent implements OnInit {
-
   genres: any = [];
   @Output() searchEvent = new EventEmitter<string>();
   @Output() discoverEvent = new EventEmitter<NgForm>();
-
+  hiddenSearch: boolean = false;
 
   constructor(private movieService: MovieService) {}
 
   ngOnInit(): void {
     this.getGenres();
   }
-  
+
   emitSearch = (form: NgForm) => {
     console.log(form);
     this.searchEvent.emit(form.form.value.searchTerm);
     this.searchEvent.emit(form.form.value.genre);
     this.searchEvent.emit(form.form.value.rating);
   };
-  emitDiscoverEvent = (form: NgForm):void => {
+  emitDiscoverEvent = (form: NgForm): void => {
     this.discoverEvent.emit(form);
   };
-  getGenres = ():void => {
-    this.movieService.getGenres().subscribe((response)=>{
+  getGenres = (): void => {
+    this.movieService.getGenres().subscribe((response) => {
       this.genres = response;
       console.log(response);
     });
-  }
+  };
+  toggleSearch = (): void => {
+    this.hiddenSearch = !this.hiddenSearch;
+  };
 }
